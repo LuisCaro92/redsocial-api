5296ebc101ae8b2c9c3a58820954137b50155009
luis caro <luis.carosalgado1@gmail.com>
2023-06-03 16:25:29 -0400

    esperando mejoras

--- src/views/card.js
+++ src/views/card.js
@@ -3,7 +3,8 @@
 
 const Card = (props) => {
     return (
-        <div className="card d-flex justify-content-center" style={{"width":400,"height":500 }}>
+        <div className="card d-flex justify-content-center" style={{"width":400,"height":600 }}>
+            <div className="card-heaer"><img src={props.avatar} style={{"width":50}}/> <p>{props.name}</p></div>
             <img src={props.images} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
--- src/views/home.js
+++ src/views/home.js
@@ -8,37 +8,48 @@ import { useEffect, useState } from "react";
 const Home = () => {
 
     const [productos, setProductos] = useState([]);
-
+    const [users, setUsers] = useState([]);
 
 
     const getProductos = () => {
-        fetch("https://api.escuelajs.co/api/v1/products")
+        fetch("https://api.themoviedb.org/3/account/19701745")
             .then((res) => res.json())
-            .then((data) => setProductos(data))
+            .then((data) => console.log(data))
             .catch((error) => console.log(error));
-            
+
     }
-   
+
+    const getusers = () => {
+        fetch("https://api.escuelajs.co/api/v1/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data))
+            .catch((error) => console.log(error))
+    }
     useEffect(() => {
         getProductos();
+        getusers();
     })
 
 
     return (
         <div className="">
             <div className="row">
-            {productos.map((producto) => (
-                <div className="col-3 m-5">
-                    <Card
-                        images={producto.images}
-                        title={producto.title}
-                        description= {producto.description}
-                    />
-                </div>
-            ))}
-          </div>
+                
+                {productos.map((producto, user) => (
+                    <div className="col-3 m-5">
+                        <Card
+                            name={user.name}
+                            avatar={user.avatar}
+                            images={producto.images}
+                            title={producto.title}
+                            description={producto.description}
+                        />
+                    </div>
+                ))}
+               
+            </div>
         </div>
     )
-}
+} 
 
 export default Home;
